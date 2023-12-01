a61367456d008e2446be6a6f8c9f238381bd74d0
najibdj_ <najib.djulfikar@gmail.com>
2023-12-01 09:03:18 +0700

    create skeleton component

---
+++ screens/components/Skeleton.js
@@ -0,0 +1,46 @@
+
+import { View, Animated, StyleSheet } from 'react-native'
+import { React, useEffect, useRef } from 'react'
+import { LinearGradient } from 'expo-linear-gradient';
+const Skeleton = ({width, height, style}) => {
+    const translateX = useRef(new Animated.Value(-width)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.timing(translateX, {
+                toValue: width,
+                useNativeDriver: true,
+                duration: 1000
+            })
+        ).start();
+    }, [width])
+    return (
+        <View
+            style={StyleSheet.flatten([
+                {
+                    width: width, 
+                    height: height, 
+                    backgroundColor: "rgba(0,0,0,0.12)",
+                    overflow: "hidden"
+                },
+                style
+            ])}
+        >
+            <Animated.View
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    transform: [{translateX: translateX}]
+                }}
+            >
+                <LinearGradient 
+                    style={{ width: "100%", height: "100%" }}
+                    colors={["transparent", "rgba(0,0,0,0.05)", "transparent"]}
+                    start={{x: 1, y: 1}}
+                />
+            </Animated.View>
+        </View>
+    );
+}
+
+export default Skeleton;
