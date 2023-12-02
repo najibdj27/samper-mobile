ecdf553c02942cbb062fad457991b202db040aea
najibdj_ <najib.djulfikar@gmail.com>
2023-12-02 17:36:08 +0700

    impplement ComingSoon in chat page

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,14 @@
-import { View } from "react-native";
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import ComingSoon from './components/ComingSoon'
 
 const ChatScreen = () => {
+    const comingSoonImg = require("../../samper-mobile/assets/35020246_8262266.jpg")
     return (
-        <View>
-
-        </View>
-    );
+        <ComingSoon imageSource={comingSoonImg} />
+    )
 }
 
 export default ChatScreen;
+
+const styles = StyleSheet.create({})
