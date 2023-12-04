aae1bd37e6bb6bd36df7d16ee6f105ae3e2da143
najibdj_ <najib.djulfikar@gmail.com>
2023-12-05 06:46:16 +0700

    implement dynamic data

--- screens/components/Request.js
+++ screens/components/Request.js
@@ -2,12 +2,47 @@ import { Image, StyleSheet, View, useWindowDimensions } from 'react-native'
 import React from 'react'
 import { Avatar, Button, Card, Text } from 'react-native-paper';
 import Skeleton from './Skeleton';
+import moment from 'moment';
 
 const Request = ({item, isLoading, isEmpty}) => {
   // #03913E #D8261D #F8C301
-  const LeftContent = props => <Avatar.Icon {...props} icon="clipboard-text-clock" color='#fff' style={{backgroundColor: '#F8C301'}} />
+  const statusColor = (status) => {
+    switch (status) {
+      case 'P':
+        return {backgroundColor: '#F8C301'}
+        break;
+      case 'A':
+        return {backgroundColor: '#03913E'}
+        break;
+      case 'R':
+        return {backgroundColor: '#D8261D'}
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  const statusType = (type) => {
+    switch (type) {
+      case 'C':
+          return "clock-edit-outline"
+        break;
+      case 'P':
+          return "calendar-sync"
+        break;
+      case 'R':
+          return "calendar-clock"
+        break;
+        
+      default:
+        break;
+    }
+  }
+
+  const LeftContent = props => <Avatar.Icon {...props} icon="clipboard-text-clock" color='#fff' style={statusColor(item.status)} />
   // 
-  const RightContent = props => <Avatar.Icon {...props} icon="clock-edit-outline" color='#000' size={40} style={{backgroundColor: "rgba(0,0,0,0)"}} />
+  const RightContent = props => <Avatar.Icon {...props} icon={statusType(item.type)} color='#000' size={40} style={{backgroundColor: "rgba(0,0,0,0)"}} />
 
   const {width} = useWindowDimensions()
 
@@ -15,11 +50,11 @@ const Request = ({item, isLoading, isEmpty}) => {
 
   const requestAvailable = () => {
     return (
-      <Card style={{marginBottom: 5, marginHorizontal: 5, paddingEnd: 5}}>
-          <Card.Title title="Sistem Pengambilan Keputusan" subtitle="1 Desember 2023 | 22:20" left={LeftContent} right={RightContent} titleStyle={{fontWeight: "bold"}} />
+      <Card style={{marginBottom: 10, marginHorizontal: 5, paddingEnd: 5}}>
+          <Card.Title title={item.subjectName} subtitle={`${moment(item.timestamp, "YYYY-MM-DD hh:mm").format("D MMMM YYYY | hh:mm")}`} left={LeftContent} right={RightContent} titleStyle={{fontWeight: "bold"}} />
           <Card.Content>
               <Text variant="titleMedium">Reason</Text>
-              <Text variant="bodyMedium">Menjenguk keluarga yang sakit</Text>
+              <Text variant="bodyMedium">{item.reason}</Text>
           </Card.Content>
       </Card>
     )
