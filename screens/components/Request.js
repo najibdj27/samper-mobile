fb707436838213e508e29c8be1b35582e76346e0
najibdj_ <najib.djulfikar@gmail.com>
2023-12-04 20:50:12 +0700

    create request element

--- screens/components/Request.js
+++ screens/components/Request.js
@@ -1,12 +1,17 @@
-import { StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, View, useWindowDimensions } from 'react-native'
 import React from 'react'
 import { Avatar, Button, Card, Text } from 'react-native-paper';
 import Skeleton from './Skeleton';
 
 const Request = ({item, isLoading, isEmpty}) => {
-  // #03913E #D8261D
-  const LeftContent = props => <Avatar.Icon {...props} icon="clipboard-text-clock" color='#fff' style={{backgroundColor: '#D8261D'}} />
+  // #03913E #D8261D #F8C301
+  const LeftContent = props => <Avatar.Icon {...props} icon="clipboard-text-clock" color='#fff' style={{backgroundColor: '#F8C301'}} />
+  // 
   const RightContent = props => <Avatar.Icon {...props} icon="clock-edit-outline" color='#000' size={40} style={{backgroundColor: "rgba(0,0,0,0)"}} />
+
+  const {width} = useWindowDimensions()
+
+  const emptyImg = require('../../assets/7732624_5251.jpg')
 
   const requestAvailable = () => {
     return (
@@ -21,28 +26,47 @@ const Request = ({item, isLoading, isEmpty}) => {
   }
 
   const requestLoading = () => {
-    return (
-      <Card style={{marginBottom: 5, marginHorizontal: 5}}>
-          <View style={{flexDirection: "row", justifyContent:"space-between", marginVertical: 16, marginHorizontal: 16}}>
-              <View style={{flexDirection: 'row'}}>
-                <Skeleton width={40} height={40} style={{borderRadius: 20}} />
-                <View style={{marginStart: 15}}>
-                  <Skeleton width={250} height={18} style={{borderRadius: 10, marginBottom: 10}} />
-                  <Skeleton width={200} height={12} style={{borderRadius: 10}} />
+    const element = []
+    const loopLength = 6
+    for (let index = 0; index < loopLength; index++) {
+      element.push(
+        <Card key={index} style={{marginBottom: 5, marginHorizontal: 5}}>
+            <View style={{flexDirection: "row", justifyContent:"space-between", marginVertical: 16, marginHorizontal: 16}}>
+                <View style={{flexDirection: 'row'}}>
+                  <Skeleton width={40} height={40} style={{borderRadius: 20}} />
+                  <View style={{marginStart: 15}}>
+                    <Skeleton width={250} height={18} style={{borderRadius: 10, marginBottom: 10}} />
+                    <Skeleton width={200} height={12} style={{borderRadius: 10}} />
+                  </View>
                 </View>
-              </View>
-              <Skeleton width={24} height={24} style={{borderRadius: 15, marginTop: 8, marginEnd: -3}} />
-          </View>
-          <View style={{marginBottom: 16, marginHorizontal: 16}}>
-            <Skeleton width={80} height={15} style={{borderRadius: 15, marginTop: 8, marginEnd: -3}} />
-            <Skeleton width={200} height={13} style={{borderRadius: 15, marginTop: 8, marginEnd: -3}} />
-          </View>
-      </Card>
+                <Skeleton width={24} height={24} style={{borderRadius: 15, marginTop: 8, marginEnd: -3}} />
+            </View>
+            <View style={{marginBottom: 16, marginHorizontal: 16}}>
+              <Skeleton width={80} height={12} style={{borderRadius: 15, marginTop: 8, marginEnd: -3}} />
+              <Skeleton width={200} height={10} style={{borderRadius: 15, marginTop: 8, marginEnd: -3}} />
+            </View>
+        </Card>
+      )
+    }
+    return (
+      <View>
+        {element}
+      </View>
+    )
+  }
+
+  const requestEmpty = () => {
+    return (
+      <View style={{justifyContent: "center", alignItems: "center", marginTop: 80}}>
+        <Image source={emptyImg} style={{height: 200, width: width}} />
+        <Text style={{fontSize: 28, fontWeight: "bold"}}>
+          Request is empty!
+        </Text>
+      </View>
     )
   }
   
-  return requestAvailable()
-  // return requestLoading()
+  return isLoading? requestLoading() : isEmpty? requestEmpty() : requestAvailable()
 }
 
 export default Request
