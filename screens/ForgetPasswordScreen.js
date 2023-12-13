8e976f67919231e4338040a84c967f05ded409e1
najibdj_ <najib.djulfikar@gmail.com>
2023-12-14 02:32:57 +0700

    implement api call to send otp by email

--- screens/ForgetPasswordScreen.js
+++ screens/ForgetPasswordScreen.js
@@ -1,68 +1,89 @@
 import * as React from 'react';
-import { Pressable, StyleSheet, Image, Keyboard } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { Pressable, StyleSheet, Image, Keyboard, View } from "react-native";
+import { Button, PaperProvider, Portal, Provider, Text, TextInput } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import useAPI from './hooks/useAPI';
-import axios from 'axios';
 import Loader from './components/Loader';
+import DialogMessage from './components/DialogMessage';
 
 function ForgetPasswordScreen(){
     const [email, setEmail] = React.useState("");
+    const [screenLoading, setScreenLoading] = React.useState();
 
+    const dialogRef = React.useRef()
+    const loaderRef = React.useRef()
     const navigation = useNavigation();
     const [response, isLoading, isSuccess, errorCode, errorMessage, callAPI] = useAPI();
     const reqBody ={emailAddress: email}
 
+    React.useEffect(() => {
+        if (screenLoading) {
+            loaderRef.current.showLoader()
+        }else{
+            loaderRef.current.hideLoader()
+        }
+    }, [screenLoading])
+
+    React.useEffect(() => {
+        if (isLoading) {
+            setScreenLoading(true)
+        }else{
+            setScreenLoading(false)
+        }
+    }, [isLoading])
+
     const handleSendOtp = async () => {
         const successCallback = () => {
             navigation.navigate('ForgetPasswordOtp', reqBody)
+            setEmail()
         }
-        await callAPI('post', '/auth/forgetpassword', reqBody, null, successCallback, null)
+        const errorCallback = () => {
+            dialogRef.current.showDialog('error')
+        }
+        await callAPI('post', '/auth/forgetpassword', reqBody, null, successCallback, errorCallback)
         
     }
 
     const topImg = require("../assets/76fa4704-6ac7-4e25-bf37-68479913878f.png")
 
     return(
-        <Pressable style={styles.container} onPress={Keyboard.dismiss}>
-            {
-                isLoading? 
-                    <Loader />
-                    :
-                    null
-            }
-            <Image source={topImg} style={{width: 320, height: 260}} />
-            <Text variant="titleLarge" style={styles.titleText}>
-                We will send OTP code to your email!
-            </Text>
-            <TextInput
-                label="Email"
-                placeholder='Input your email here'
-                value={email}
-                mode='outlined'
-                activeOutlineColor='#02a807'
-                inputMode='email'
-                keyboardType='email-address'
-                style={styles.form}
-                outlineStyle={{borderRadius:16}}
-                onChangeText={text => setEmail(text)}
-            />
-            <Button 
-                icon="login" 
-                mode="contained" 
-                style={styles.button} 
-                contentStyle={styles.buttonContent} 
-                buttonColor="#03913E"
-                onPress={handleSendOtp}
-                labelStyle={{
-                    fontSize: 18, 
-                    fontWeight: "bold"
-                }}
-            >
-                Send OTP
-            </Button>
-        </Pressable>
-    );
+        <PaperProvider>
+            <Pressable style={styles.container} onPress={Keyboard.dismiss}>
+                <Image source={topImg} style={{width: 320, height: 260}} />
+                <Text variant="titleLarge" style={styles.titleText}>
+                    We will send OTP code to your email!
+                </Text>
+                <TextInput
+                    label={<Text>Email</Text>}
+                    placeholder='Input your email here'
+                    value={email}
+                    mode='outlined'
+                    activeOutlineColor='#02a807'
+                    inputMode='email'
+                    keyboardType='email-address'
+                    style={styles.form}
+                    outlineStyle={{borderRadius:16}}
+                    onChangeText={text => setEmail(text)}
+                />
+                <Button 
+                    icon="login" 
+                    mode="contained" 
+                    style={styles.button} 
+                    contentStyle={styles.buttonContent} 
+                    buttonColor="#03913E"
+                    onPress={handleSendOtp}
+                    labelStyle={{
+                        fontSize: 18, 
+                        fontWeight: "bold"
+                    }}
+                >
+                    Send OTP
+                </Button>
+            </Pressable>
+            <Loader ref={loaderRef} />
+            <DialogMessage errorCode={errorCode} errorMessage={errorMessage} ref={dialogRef} />
+        </PaperProvider>
+);
 }
 
 const styles = StyleSheet.create({
