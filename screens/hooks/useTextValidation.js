7207bb64eec0c7154a1f14503bc82b6074b27122
najibdj_ <najib.djulfikar@gmail.com>
2024-01-01 10:36:13 +0700

    add validation for new password

--- screens/hooks/useTextValidation.js
+++ screens/hooks/useTextValidation.js
@@ -16,10 +16,42 @@ const useTextValidation = () => {
         }
     }
 
+    const newPassword = (input) => {
+        const regEx1 = new RegExp(/^(?=.*[A-Z].*[A-Z]).+$/g) // must at least have 1 uppercase letter
+        const regEx2 = new RegExp(/^(?=.*[a-z].*[a-z].*[a-z]).+$/g) // must at least have 3 lowercase letters
+        const regEx3 = new RegExp(/^(?=.*[0-9].*[0-9]).+$/g) // must at least have 2 numbers
+        const regEx4 = new RegExp(/^(?=.*[!@#$&*]).+$/g) // must at least have 1 special character '!@#$&*'
+        if (regEx1.test(input)) {
+            if (regEx2.test(input)) {
+                if (regEx3.test(input)) {
+                    if (regEx4.test(input)) {
+                        setIsValid(true)
+                    } else {
+                        setIsValid(false)
+                        setMessage(`Your password must at least have 1 special character '!@#$&*'`)
+                    }
+                } else {
+                    setIsValid(false)
+                    setMessage(`Your password  must at least have 2 numbers`)
+                }
+            } else {
+                setIsValid(false)
+                setMessage(`Your password must at least have 3 lowercase letters`)
+            }
+        } else {
+            setIsValid(false)
+            setMessage(`Your password must at least have 1 uppercase letter`)
+        }
+    }
+
     const validate = (type, input) => {
         switch (type) {
             case 'email':
                 email(input)
+                break;
+            
+            case 'newPassword':
+                newPassword(input)
                 break;
         
             default:
