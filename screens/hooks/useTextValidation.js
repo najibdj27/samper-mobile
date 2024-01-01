cd0b9be3bd5656a24a8a8fd10d5c8536be4c00e2
najibdj_ <najib.djulfikar@gmail.com>
2024-01-01 14:52:59 +0700

    fix regex for password validation

--- screens/hooks/useTextValidation.js
+++ screens/hooks/useTextValidation.js
@@ -17,10 +17,10 @@ const useTextValidation = () => {
     }
 
     const newPassword = (input) => {
-        const regEx1 = new RegExp(/^(?=.*[A-Z].*[A-Z]).+$/g) // must at least have 1 uppercase letter
+        const regEx1 = new RegExp(/^(?=.*[A-Z]).+$/g) // must at least have 1 uppercase letter
         const regEx2 = new RegExp(/^(?=.*[a-z].*[a-z].*[a-z]).+$/g) // must at least have 3 lowercase letters
         const regEx3 = new RegExp(/^(?=.*[0-9].*[0-9]).+$/g) // must at least have 2 numbers
-        const regEx4 = new RegExp(/^(?=.*[!@#$&*]).+$/g) // must at least have 1 special character '!@#$&*'
+        const regEx4 = new RegExp(/^(?=.*[!@#$&*.]).+$/g) // must at least have 1 special character '!@#$&*'
         if (regEx1.test(input)) {
             if (regEx2.test(input)) {
                 if (regEx3.test(input)) {
