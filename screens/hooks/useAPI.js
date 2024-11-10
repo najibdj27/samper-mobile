0c029247d5369568d11a658195381732494405b9
najibdj_ <najib.djulfikar@gmail.com>
2024-11-10 21:47:57 +0700

    change timeout

--- screens/hooks/useAPI.js
+++ screens/hooks/useAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const useAPI = async (auth, method, url, request, param, accessToken) => {
     return await axios({
         baseURL: 'http://192.168.8.23:8080',
-        timeout: 30000,
+        timeout: 70000,
         headers: {
             'Authorization': `Bearer ${accessToken}`
         },
