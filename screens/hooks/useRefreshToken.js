7c54c8eb9a0c4a2d637b7f709c151d22eed30914
najibdj_ <najib.djulfikar@gmail.com>
2024-11-23 20:24:43 +0700

    get from storage if state not ready

--- screens/hooks/useRefreshToken.js
+++ screens/hooks/useRefreshToken.js
@@ -1,15 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import useAuth from './useAuth'
 import usePublicCall from './usePublicCall'
 
 const useRefreshToken = () => {
 
     const axiosPublic = usePublicCall()
-    const {authState, setAuthState, logout} = useAuth()
+    const {authState, setAuthState} = useAuth()
 
     const refresh = async () => {
+        let refreshToken
+
+        if (authState?.refreshToken !== null ) {
+            console.log('refreshToken: get from state')
+            refreshToken = authState.refreshToken
+        }else {
+            console.log('refreshToken: get from storage')
+            refreshToken = await AsyncStorage.getItem('refreshToken')
+        }
+        
+        console.log(`refreshToken: ${refreshToken}`)
+
         const response = await axiosPublic.post('/auth/refreshtoken', 
             {
-                refreshToken: authState.refreshToken 
+                refreshToken: refreshToken 
             },
             {
                 withCredentials: true
