9020b344af26edca6b6723340658671162236e12
najibdj_ <najib.djulfikar@gmail.com>
2024-11-27 17:21:18 +0700

    move initialation to inital screen

--- screens/navigators/AppNav.js
+++ screens/navigators/AppNav.js
@@ -1,25 +1,12 @@
-import React, { useContext } from 'react'
-import { NavigationContainer } from '@react-navigation/native';
+
 import { PaperProvider } from 'react-native-paper';
-import { AuthContext } from '../contexts/AuthContext';
-import AuthStack from './AuthStack';
-import AnonymousStack from './AnonymousStack';
 import InitialLoadingScreen from '../InitialLoadingScreen';
 
 const AppNav = () => {
-    const auth = useContext(AuthContext);
 
     return (
         <PaperProvider>
-            <NavigationContainer>
-                {
-                    auth.authState.isLoading?
-                    <InitialLoadingScreen /> :
-                        auth.authState.isAuthenticated ? 
-                        <AuthStack /> : 
-                        <AnonymousStack />
-                }
-            </NavigationContainer>
+            <InitialLoadingScreen />
         </PaperProvider>
     )
 }
