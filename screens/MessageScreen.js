13dd33d7065dacde7d20487f386652aa244a905a
najibdj_ <najib.djulfikar@gmail.com>
2024-11-27 16:53:50 +0700

    fix image path

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -3,12 +3,10 @@ import React from 'react'
 import ComingSoon from './components/ComingSoon'
 
 const MessageScreen = () => {
-    const comingSoonImg = require("../../samper-mobile/assets/35020246_8262266.jpg")
+    const comingSoonImg = require("../assets/35020246_8262266.jpg")
     return (
         <ComingSoon imageSource={comingSoonImg} />
     )
 }
 
 export default MessageScreen;
-
-const styles = StyleSheet.create({})
