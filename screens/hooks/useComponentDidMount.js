bab766fb21456d525c5f149ad724ac5745467206
najibdj_ <najib.djulfikar@gmail.com>
2024-11-27 15:25:00 +0700

    remove unused hooks

--- screens/hooks/useComponentDidMount.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from "react";
-
-export const useComponentDidMount = () => {
-    const ref = useRef();
-    useEffect(() => {
-      ref.current = true;
-    }, []);
-    return ref.current;
-};
