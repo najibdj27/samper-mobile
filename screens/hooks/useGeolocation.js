1a09aebb1763bda518aec8a61434c4e83e709e19
najibdj_ <najib.djulfikar@gmail.com>
2025-05-27 06:23:12 +0700

    remove logger

--- screens/hooks/useGeolocation.js
+++ screens/hooks/useGeolocation.js
@@ -7,28 +7,19 @@ export default useGeolocation = () => {
     const [latitudeCoords, setLatitudeCoords] = useState(null)
 
     const { loaderOn, loaderOff, showDialogConfirmation, hideDialogConfirmation } = useModal()
-    console.log(`geo hook render`)
-    console.log(`Longitude: ${longitudeCoords}`)
-    console.log(`Latitude: ${latitudeCoords}`)
 
     useEffect(() => {
         const getCurrentLocation = async () => {
-            console.log(`getCurrentLocation`)
             let {status} = await Location.requestForegroundPermissionsAsync()
             
             if (status !== 'granted') {
-                console.log(`Location access not granted`)
                 showDialogConfirmation('error', 'Location Permission', 'Can not access current location', null, () => {hideDialogConfirmation()})
                 return;
             }
             
             loaderOn() 
             await Location.getCurrentPositionAsync().then((resp) => {
-                console.log(`success getCurrentLocation`)
-                const {latitude, longitude} = resp.coords;
-                console.log(`Longitude: ${longitude}`)
-                console.log(`Latitude: ${latitude}`)
-                
+                const {latitude, longitude} = resp.coords;                
                 setLongitudeCoords(longitude)
                 setLatitudeCoords(latitude)
             }).catch((err) => {
