1951bb26d19ab40f1618926af3ad9d735b815a93
najibdj_ <najib.djulfikar@gmail.com>
2025-06-01 22:50:52 +0700

    add the key to children list

--- screens/components/EmptyAgendaCard.js
+++ screens/components/EmptyAgendaCard.js
@@ -3,7 +3,7 @@ import { Image, View } from "react-native";
 import { Card, Text } from 'react-native-paper';
 
 
-const EmptyAgendaCard = () => {
+const EmptyAgendaCard = (props) => {
     const freeImg = require("../../assets/3f8f3f29-ea13-41c3-84bd-ad82927a6626.png")
 
     return (
