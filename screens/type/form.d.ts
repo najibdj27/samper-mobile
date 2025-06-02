c07e65f28a428ef19d9a040bf5f6f65ddb7ad868
najibdj_ <najib.djulfikar@gmail.com>
2025-06-02 17:40:29 +0700

    change file extention to .d.ts

---
+++ screens/type/form.d.ts
@@ -0,0 +1,13 @@
+export type SignUpFormDataType = {
+    firstName?: string
+    lastName?: string
+    dateOfBirth?: string
+    nim?: number
+    nip?: number
+    username?: string
+    email?: string
+    phoneNumber?: number
+    password?: string
+    class?: number
+    faceData?: string
+}
