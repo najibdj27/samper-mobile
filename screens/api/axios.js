8b15a5a0cddb0e26bd3c9d41b961684d50596a29
najibdj_ <najib.djulfikar@gmail.com>
2025-06-08 22:42:12 +0700

    set timeout to 70000ms

--- screens/api/axios.js
+++ screens/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://samper-api-6bf01e4f0b51.herokuapp.com';
-const BASE_URL = 'http://192.168.48.227:8081';
-const TIMEOUT = 70
+const BASE_URL = 'https://samper-api-6bf01e4f0b51.herokuapp.com';
+const TIMEOUT = 70000
 
 export const axiosPrivateCall = axios.create({
     baseURL: BASE_URL,
