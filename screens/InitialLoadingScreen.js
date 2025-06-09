2aea65bb294851d7118c216f76ff6f7ca1ea0c69
najibdj_ <najib.djulfikar@gmail.com>
2025-06-09 08:45:05 +0700

    reset spalsh loading size

--- screens/InitialLoadingScreen.js
+++ screens/InitialLoadingScreen.js
@@ -75,8 +75,8 @@ const InitialLoadingScreen = () => {
             {
                 authState.isAuthenticated === undefined? 
                 (
-                    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-                        <Image source={spalshImg} />
+                    <View style={{flex: 1, backgroundColor: 'white', justifyContent: "center", alignItems: "center"}}>
+                        <Image style={{width: 200, height: 200}} source={spalshImg} />
                     </View>
                 ) 
                 :
