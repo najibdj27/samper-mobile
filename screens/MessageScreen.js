cb241648f6049c9559979cc295fcfa7fe45f63bf
najibdj_ <najib.djulfikar@gmail.com>
2025-06-20 11:34:37 +0700

    add statusbar color

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,11 +1,14 @@
-import { StyleSheet } from 'react-native'
+import { Platform, StatusBar, StyleSheet, View } from 'react-native'
 import React from 'react'
 import ComingSoon from './components/ComingSoon'
 
 const MessageScreen = () => {
     const comingSoonImg = require("../assets/35020246_8262266.jpg")
     return (
-        <ComingSoon imageSource={comingSoonImg} />
+        <>
+            <View style={{backgroundColor: '#D8261D',  paddingTop: Platform.OS === 'android'? StatusBar.currentHeight : 0, width: '100%'}} />
+            <ComingSoon imageSource={comingSoonImg} />
+        </>
     )
 }
 
--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -1,10 +1,13 @@
-import { View } from "react-native";
+import { Platform, StatusBar, View } from "react-native";
 import ComingSoon from "./components/ComingSoon";
 
 const NotificationScreen = () => {
     const notificationComingSoonImg = require("../assets/19245722_6101668.jpg")
     return (
-        <ComingSoon imageSource={notificationComingSoonImg} />
+        <>
+            <View style={{backgroundColor: '#D8261D',  paddingTop: Platform.OS === 'android'? StatusBar.currentHeight : 0, width: '100%'}} />
+            <ComingSoon imageSource={notificationComingSoonImg} />
+        </>
     );
 }
 
