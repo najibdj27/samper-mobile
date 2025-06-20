4670aefe32068b592f44302d89cc643eeb24e4fe
najibdj_ <najib.djulfikar@gmail.com>
2025-06-20 11:28:50 +0700

    set piloting

--- screens/PresenceDataScreen.tsx
+++ screens/PresenceDataScreen.tsx
@@ -1,7 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import PilotingModal from './components/PilotingModal'
+
+const isPiloting = true
+
 
 const PresenceDataScreen = () => {
+
+  if (isPiloting) {
+    return <PilotingModal isVisible={true} redirectScreen='BACK'   />
+  }
+
   return (
     <View>
       <Text>PresenceDataScreen</Text>
