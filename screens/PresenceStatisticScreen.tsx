3d49b4fd7973ae2e161814d10a584e2b7343250a
najibdj_ <najib.djulfikar@gmail.com>
2025-06-20 11:25:58 +0700

    set piloting

--- screens/PresenceStatisticScreen.tsx
+++ screens/PresenceStatisticScreen.tsx
@@ -1,7 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import PilotingModal from './components/PilotingModal'
+
+const isPiloting = true
 
 const PresenceStatisticScreen = () => {
+
+  if (isPiloting) {
+    return <PilotingModal isVisible={true} redirectScreen='BACK'   />
+  }
+
   return (
     <View>
       <Text>PresenceStatisticScreen</Text>
