5ebb8bc10579745e5fa9f73e4c672924f05a023e
najibdj_ <najib.djulfikar@gmail.com>
2025-08-26 11:00:07 +0700

    add rn-config

--- screens/api/axios.js
+++ screens/api/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://samper-api-6bf01e4f0b51.herokuapp.com';
-const BASE_URL = 'http://192.168.3.106:8080';
+const BASE_URL = 'https://samper-api-6bf01e4f0b51.herokuapp.com';
 const TIMEOUT = 70000
 
 export const axiosPrivateCall = axios.create({
